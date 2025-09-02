f7bd26024de9580e478bd0b7b5d00d645c82e07e
morrislee <morrismurphy@gmail.com>
2025-09-01 22:14:53 -0600

    Sierra Tune

--- opendbc_repo/opendbc/car/gm/interface.py
+++ opendbc_repo/opendbc/car/gm/interface.py
@@ -18,7 +18,7 @@ NetworkLocation = structs.CarParams.NetworkLocation
 NON_LINEAR_TORQUE_PARAMS = {
   CAR.CHEVROLET_BOLT_EUV: [2.6531724862969748, 1.0, 0.1919764879840985, 0.009054123646805178],
   CAR.GMC_ACADIA: [4.78003305, 1.0, 0.3122, 0.05591772],
-  CAR.CHEVROLET_SILVERADO: [3.29974374, 1.0, 0.25571356, 0.0465122]
+  CAR.CHEVROLET_SILVERADO: [3.8, 0.81, 0.21, 0.0465122]
 }
 
 
@@ -196,7 +196,16 @@ class CarInterface(CarInterfaceBase, CarInterfaceExt):
       # TODO: check if this is split by EV/ICE with more platforms in the future
       if ret.openpilotLongitudinalControl:
         ret.minEnableSpeed = -1.
+        ret.stopAccel = -0.5
+        ret.stoppingDecelRate = 1.5  # reach brake quickly after enabling
+        ret.vEgoStopping = 0.5
+        ret.vEgoStarting = 0.5
+        ret.longitudinalActuatorDelay = 0.3
+        #ret.longitudinalTuning.kf = 1.05
+        ret.longitudinalTuning.kiV = [0.7, 0.7]
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
+      ret.steerActuatorDelay = 0.37
+      ret.minSteerSpeed = -1
 
     elif candidate == CAR.CHEVROLET_EQUINOX:
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
