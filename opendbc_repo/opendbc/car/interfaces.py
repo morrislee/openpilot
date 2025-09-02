d9cf316e30198c532ea87d5dddbc821989069163
morrislee <morrismurphy@gmail.com>
2025-09-01 22:18:35 -0600

    wheel

--- opendbc_repo/opendbc/car/interfaces.py
+++ opendbc_repo/opendbc/car/interfaces.py
@@ -146,6 +146,7 @@ class CarInterfaceBase(ABC, CarInterfaceBaseSP):
     ret.minEnableSpeed = platform.config.specs.minEnableSpeed
     ret.minSteerSpeed = platform.config.specs.minSteerSpeed
     ret.tireStiffnessFactor = platform.config.specs.tireStiffnessFactor
+    ret.wheelRadius = platform.config.specs.wheelRadius
     ret.flags |= int(platform.config.flags)
 
     ret = cls._get_params(ret, candidate, fingerprint, car_fw, alpha_long, is_release, docs)
